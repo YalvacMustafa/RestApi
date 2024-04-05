20d781912fe70ab651e28bba35767d283cf37749
YalvacMustafa <zapza574@gmail.com>
2024-04-06 00:32:25 +0300

    add gitignore and update static files

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,60 +1,56 @@
 const User = require("../models/user");
 const CustomError = require("../helpers/error/CustomError");
 const asyncErrorWrapper = require("express-async-handler");
-const {sendJwtToClient} = require("../helpers/authorization/TokenHelpers");
-const { validateUserInput, comparePassword } = require("../helpers/input/inputHelpers")
-const register = asyncErrorWrapper (async (req, res, next) => {
+const { sendJwtToClient } = require("../helpers/authorization/TokenHelpers");
+const {
+  validateUserInput,
+  comparePassword,
+} = require("../helpers/input/inputHelpers");
+const register = asyncErrorWrapper(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
 
-    const{name, email, password, role} = req.body;
-
-    const user = await User.create({
-
-        name,
-        email,
-        password,
-        role
-    });
-    sendJwtToClient(user, res);
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+  sendJwtToClient(user, res);
 });
 const login = asyncErrorWrapper(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!validateUserInput(email, password)) {
+    return next(new CustomError("Please check your inputs", 400));
+  }
 
-    const {email, password} = req.body;
-    if (!validateUserInput(email, password)) {
-
-        return next(new CustomError("Please check your inputs", 400))
-    }
-
-    const user = await User.findOne({email}).select("+password");
-    if (!comparePassword(password, user.password)){
-
-        return next(new CustomError("Please check your credentials", 400));
-    }
-    sendJwtToClient(user, res);
+  const user = await User.findOne({ email }).select("+password");
+  if (!comparePassword(password, user.password)) {
+    return next(new CustomError("Please check your credentials", 400));
+  }
+  sendJwtToClient(user, res);
 });
 
 const logout = asyncErrorWrapper(async (req, res, next) => {
-    const { NODE_ENV } = process.env;
-    return res.status(200)
+  const { NODE_ENV } = process.env;
+  return res
+    .status(200)
     .cookie({
-
-        httpOnly : true,
-        expires : new Date(Date.now()),
-        secure : NODE_ENV === "development" ? false : true
-    }).json({
-
-        success : true,
-        message : "Logout success"
+      httpOnly: true,
+      expires: new Date(Date.now()),
+      secure: NODE_ENV === "development" ? false : true,
     })
-
+    .json({
+      success: true,
+      message: "Logout success",
+    });
 });
 const getUser = (req, res, next) => {
-    res.json({
-        success : true,
-        data : {
-            id : req.user.id,
-            name : req.user.name 
-        }
-    })
-}
-module.exports = {register, getUser, login, logout};
-
+  res.json({
+    success: true,
+    data: {
+      id: req.user.id,
+      name: req.user.name,
+    },
+  });
+};
+module.exports = { register, getUser, login, logout };
